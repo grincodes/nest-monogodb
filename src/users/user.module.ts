8381bfedd1591f8ca54bbedc6611f7edd6fb2677
designerseyi <designerseyi@gmail.com>
2023-01-05 00:03:27 +0100

    nest mongodb

---
+++ src/users/user.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+import { UsersController } from './user.controller';
+import { UsersRepository } from './user.repository';
+import { UsersService } from './user.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+})
+export class UserModule {}
---
+++ src/users/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './user.repository';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from './schemas/user.schema';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async getUserById(userId: string): Promise<User> {
+    return this.usersRepository.findOne({ userId });
+  }
+  async getUsers(): Promise<User[]> {
+    return this.usersRepository.find({});
+  }
+
+  async createUser(email: string, age: number): Promise<User> {
+    return this.usersRepository.create({
+      userId: uuidv4(),
+      email,
+      age,
+      favoriteFoods: [],
+    });
+  }
+
+  async updateUser(userId: string, userUpdates: UpdateUserDto): Promise<User> {
+    return this.usersRepository.findOneAndUpdate({ userId }, userUpdates);
+  }
+}
